b913d12e937099b947bdad498904caf2a421cecb
masud-rana44 <mdmasudrana4488@gmail.com>
2023-09-24 21:05:04 +0600

    tailwind and react router setup completed

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-indigo-800 font-bold text-2xl">
+      <h2 className="text-sm font-bold text-rose-500">Masud Rana</h2>
+    </div>
+  );
+}
+
+export default App;
